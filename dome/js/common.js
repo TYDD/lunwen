d496bc4536e67bad38e3dc9c1d36a7108ecb56f4
陶宇弟 <41106250+TYDD@users.noreply.github.com>
2019-03-03 21:37:20 +0800

    Add files via upload

---
+++ dome/js/common.js
@@ -0,0 +1,22 @@
+$(function(){
+    // 初始化
+    bannerInit()
+    console.log(window.innerHeight);//浏览器高度
+    console.log($('.swiper-slide').height());//浏览器高度
+})
+
+
+// 初始化 banner
+
+function bannerInit(){
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: true,//可选选项，自动滑动
+        loop : true,//无缝循环
+        preventClicks : false,//默认true 点击滑动时禁止跳转
+        pagination: {
+            el: '.swiper-pagination',
+            bulletElement : 'li',
+            clickable :true,
+        },
+    })
+}
